070c6ff36d9403f04e511ee23178a4620c47c8d9
Alexei Garban <garban.valdeon@gmail.com>
2025-01-29 10:47:39 +0100

    update link

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,9 +10,17 @@ const compat = new FlatCompat({
 
 });
 
-const eslintConfig = [
+/* const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+ 
+]; */
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    rules:{
+      "@typescript-eslint/no-unused-vars": "off"
+     }
+  }),
   
-];
-
+]
 export default eslintConfig;
--- src/app/Slideshow.tsx
+++ src/app/Slideshow.tsx
@@ -1,5 +1,5 @@
 "use client";
-/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import Link from "next/link";
 import React, { useState } from "react";
 
