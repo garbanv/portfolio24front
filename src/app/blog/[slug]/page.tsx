b2ec59c0b856f2a756645324ddae3b663b5a65f0
Alexei Garban <garban.valdeon@gmail.com>
2026-02-02 14:09:38 +0100

    fix: update portfolio name and description in manifest, add image metadata in blog post, and enhance project categories

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -29,11 +29,13 @@ export async function generateMetadata({
       publishedTime: post.date,
       authors: ["Alexei Garban", "agarban.com"],
       tags: [post.category],
+      images: "/AG.jpg",
     },
     twitter: {
       card: "summary_large_image",
       title: post.title,
       description: post.excerpt,
+      images: ["/AG.jpg"],
     },
   }
 }
